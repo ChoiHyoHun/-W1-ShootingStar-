027dd23cab5c3a80e8bd5aa717c6a5733ce311a0
유승훈 <82956936+sesum9@users.noreply.github.com>
2024-07-16 13:32:30 +0900

    [MapRandomSpwan]

---
+++ Assets/Scripts/MapSpwanManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
+using UnityEngine;
+
+public class MapSpwanManager : MonoBehaviour
+{
+    public GameObject[] initialMapPrefabs; // 초기 설정한 배열값
+    private List<GameObject> useMapPrefabs; // 사용할 배열
+    private Vector3 offset = new Vector3(0,-5000,0);
+    Vector3 startTargetPosition = new Vector3(0,0,0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetGameObjects();
+        GameObject drawnElement1 = DrawElement();
+        GameObject drawnElement2 = DrawElement();
+
+        drawnElement1.transform.position = startTargetPosition;
+        drawnElement2.transform.position = startTargetPosition + offset;
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ResetGameObjects(){
+        //초기 배열 현재 리스트 복사, 랜덤 수 다 뽑혔을 때 초기화 하는 거
+        useMapPrefabs = new List<GameObject>(initialMapPrefabs);
+    }
+
+    GameObject DrawElement(){
+        if(useMapPrefabs.Count == 0){
+
+            // 현재 리스트 비어있으면 초기 리스트로 초기화
+            ResetGameObjects();
+        }
+
+        //랜덤하게 뽑고 리스트 제거
+        int index = Random.Range(0, useMapPrefabs.Count);
+        GameObject drawnElement = useMapPrefabs[index];
+        useMapPrefabs.RemoveAt(index);
+        Debug.Log(index);
+
+        return drawnElement;
+    }
+
+    
+}
