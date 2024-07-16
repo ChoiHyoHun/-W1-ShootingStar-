ccfa5883f09c93b0c959ea9077ad78d8f07d478d
LeeJimin <Easymandu007@gmail.com>
2024-07-16 15:33:02 +0900

    UI 위치 수정
    UI위치 수정 및 시작 버튼 클릭 시 작동하게 변경

--- Assets/Scripts/Game/UI/MainMenu.cs
+++ Assets/Scripts/Game/UI/MainMenu.cs
@@ -6,28 +6,33 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    
+
     public string SceneToLoad;
     public Button StartButton;
     public GameObject MainMenuCanvas;
-    public bool isGameStart=false;
+    public bool isGameStart = false;
 
-    void Start() { 
+    void Start()
+    {
+        Time.timeScale = 0f;
         StartButton.onClick.AddListener(GameStart);
-        if(isGameStart==false){
+        if (isGameStart == false)
+        {
             MainMenuCanvas.SetActive(true);
         }
     }
 
-   
+
     void Update()
     {
 
     }
 
-    public void GameStart(){
+    public void GameStart()
+    {
         MainMenuCanvas.SetActive(false);
-        isGameStart=true;
+        isGameStart = true;
+        Time.timeScale = 1f;
     }
 
 }
