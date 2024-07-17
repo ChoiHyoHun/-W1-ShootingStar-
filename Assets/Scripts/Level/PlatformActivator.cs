f957e62342ae9e92fd39b406369b8383ccebe3fd
ChoiHyoHun <hh04460446@gmail.com>
2024-07-17 14:31:20 +0900

    체력1 플랫폼 폰트

--- Assets/Scripts/Level/PlatformActivator.cs
+++ Assets/Scripts/Level/PlatformActivator.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Burst.CompilerServices;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class PlatformActivator : MonoBehaviour
